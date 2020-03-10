0a11286513a3927a1a325d3b9a8da231d2fa07c3
VinylBubbles <xiongsanakee@gmail.com>
2020-03-10 12:50:34 -0600

    added buttons and began to pull data via GIPHY api

---
+++ app.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+    var topics = ["Honeybadger", "Bumblebee", "Panda", "Dog", "Chicken", "Shark"];
+
+    //creating a button and adding a class for each item in the array
+    function makeButtons(buttonArray) {
+        for (let i = 0; i < buttonArray.length; i++) {
+            var differentButton = $("<button class='btn-outline-success m-3'>");
+            differentButton.addClass("topicButtons");
+            differentButton.attr("data-name", buttonArray[i]);
+            differentButton.text(buttonArray[i]);
+            $("#buttonSpace").append(differentButton);
+        }
+    }
+    //Rendering the buttons
+    makeButtons(topics);
+
+    function getGif() {
+        var topic = $(this).attr("data-name");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=LblY3O4psUAecTqrUYsFv8dMiotbr3Tm&limit=10";
+        console.log(queryURL)
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function (response) {
+        console.log(response.data)
+        });
+    }
+    //Adding an event listener to the button
+    $(document).on("click", ".topicButtons", getGif);
+
+    //Create another for loop, create class, grab values to make into images, append to giphy
+    var result = response.data
+
+
+});
